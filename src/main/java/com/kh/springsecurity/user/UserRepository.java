ab49a7f18915c2e418589ac43436a1cf612f1cc8
khalled alkarmi <khalled_95@gmail.com>
2023-01-27 23:49:15 +0300

    add findByEmail method

--- src/main/java/com/kh/springsecurity/user/UserRepository.java
+++ src/main/java/com/kh/springsecurity/user/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> getUserByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
